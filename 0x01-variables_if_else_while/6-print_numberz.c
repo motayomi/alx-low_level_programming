4daf61cbfb769cf625fd94c847361202111bce5d
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-11 05:19:59 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int i;
+	int i;
 
-			for (i = 48; i <= 57; i++)
-					{
-								putchar (i);
-									}
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+	}
 
-				putchar ('\n');
-					return (0);
+	putchar ('\n');
+	return (0);
 }
