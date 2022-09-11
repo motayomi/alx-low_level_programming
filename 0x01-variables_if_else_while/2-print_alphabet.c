aa3062018e6a70c24fb2e4420e86a3c4e1f3ac32
motayomi <adesiyanolutayo@gmail.com>
2022-09-10 19:38:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ *  * main - print the letters of the alphabet
+ *   * Description: print the letters of the alphabet in lowercase\n
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main(void)
+{
+		int ch;
+
+			for (ch = 'a'; ch <= 'z'; ch++)
+					{
+								putchar(ch);
+
+									}
+				putchar('\n');
+
+					return (0);
+}
