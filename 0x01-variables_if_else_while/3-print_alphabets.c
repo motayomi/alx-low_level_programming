a8ad1e54693cd8d2b5d5a3791a17cdf2fe47f194
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-11 03:50:06 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,23 +2,23 @@
 #include <unistd.h>
 
 /**
- *  * main - Entry point
- *   * Description: prints alphabet in lowercase then uppercase
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Description: prints alphabet in lowercase then uppercase
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		char lower_case;
-			char upper_case;
+	char lower_case;
+	char upper_case;
 
-				for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-						{
-									putchar(lower_case);
-										}
-					for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-							{
-										putchar(upper_case);
-											}
-						putchar('\n');
-							return (0);
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	{
+		putchar(upper_case);
+	}
+	putchar('\n');
+	return (0);
 }
