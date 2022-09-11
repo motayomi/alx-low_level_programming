6062ec9172239b348ac239d144e1275dc87e6e87
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-11 05:27:26 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   * Return: 0 (Success)
- *    */
+ * main - Entry point
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-		char base_16;
+	char base_16;
 
-			for (base_16 = 48; base_16 <= 57; base_16++)
-					{
-								putchar(base_16);
-									}
-				for (base_16 = 97; base_16 <= 102; base_16++)
-						{
-									putchar(base_16);
-										}
-					putchar('\n');
-						return (0);
+	for (base_16 = 48; base_16 <= 57; base_16++)
+	{
+		putchar(base_16);
+	}
+	for (base_16 = 97; base_16 <= 102; base_16++)
+	{
+		putchar(base_16);
+	}
+	putchar('\n');
+	return (0);
 }
