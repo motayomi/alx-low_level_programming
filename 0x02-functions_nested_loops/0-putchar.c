72adf57c4190d306833d870236bcca29ff12eaef
motayomi <adesiyanolutayo@gmail.com>
2022-09-13 15:15:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <unistd.h>                                                               
-#include "main.h"                                                                 
-                                                                                  
- /**                                                                               
- * main - Entry point                                                             
- *                                                                                
- * Return: Always 0 (Success)                                                     
- */                                                                               
-                                                                                  
-int main(void)                                                                    
-{                                                                              
-	char word[8] = "_putchar";                                                
-	int i;                                                                    
-			                                                                                  
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{                                             
+	char word[8] = "_putchar";
+	int i;
+
 	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
-	_putchar('\n');                                                           
+	_putchar('\n');
 					                                                                                  
-	return (0);                                                               
+	return (0);                                               
 }
