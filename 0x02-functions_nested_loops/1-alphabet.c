ca66fd60ac383050548ccbb9065b0436171237ec
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-14 01:02:22 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,29 +1,15 @@
 #include "main.h"
-
 /**
- *
- *  * print_alphabet - print alphabet in lower case
- *
- *   *Return: void
- *
- *    */
-
-
+ * print_alphabet - print alphabet in lower case
+ *Return: void
+ */
 
 void print_alphabet(void)
-
 {
+	char lowercase;
 
-		char lowercase;
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		_putchar (lowercase);
 
-
-
-			for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-
-						_putchar (lowercase);
-
-
-
-				_putchar('\n');
-
+	_putchar('\n');
 }
