3424c65a1235cf9e1ce812af61c906a25b5322fc
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-14 01:06:14 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,49 +1,25 @@
 #include "main.h"
-
 /**
- *
- *  * print_alphabet_x10 - print alphabets in lower cases
- *
- *   * Description: print 10 times
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * print_alphabet_x10 - print alphabets in lower cases
+ * Description: print 10 times
+ * Return: Always 0 (Success)
+ */
 
 void print_alphabet_x10(void)
-
 {
 
+	int alphabet_count = 0;
 
+	while (alphabet_count < 10)
+	{
+		char ch = 'a';
 
-		int alphabet_count = 0;
-
-
-
-			while (alphabet_count < 10)
-
-					{
-
-								char ch = 'a';
-
-
-
-										while (ch <= 'z')
-
-													{
-
-																	_putchar(ch);
-
-																				ch++;
-
-																						}
-
-												alphabet_count++;
-
-														_putchar('\n');
-
-															}
-
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		alphabet_count++;
+		_putchar('\n');
+	}
 }
