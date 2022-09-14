535350624050ca9853212c7e40dc152abcba317d
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-14 01:39:51 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code
- * Return: Always 0.
+ * _abs - compute the absolute value of an integer
+ * @n: int type number
+ * Return: absolute value of @n
  */
-int main(void)
+int _abs(int n)
 {
 
-	    int r;
-
-	    r = _abs(-1);
-	    printf("%d\n", r);
-	    r = _abs(0);
-	    printf("%d\n", r);
-	    r = _abs(1);
-	    printf("%d\n", r);
-	    r = _abs(-98);
-	    printf("%d\n", r);
-	    return (0);
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
 }
