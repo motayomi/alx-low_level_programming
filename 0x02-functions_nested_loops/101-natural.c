41add262cf61871f540b349ae196e393961b8132
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-14 03:41:30 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,51 +1,26 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - list all natural numbers multiples of 3 & 5
  *
- *  * main - list all natural numbers multiples of 3 & 5
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-
+ * Return: Always 0.
+ */
 
 int main(void)
-
 {
+	int num, sum;
 
-		int num, sum;
+	sum = 0;
 
+	for (num = 0; num < 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			sum += num;
+		}
+	}
 
+	printf("%d\n", sum);
 
-			sum = 0;
-
-
-
-				for (num = 0; num < 1024; num++)
-
-						{
-
-									if (num % 3 == 0 || num % 5 == 0)
-
-												{
-
-																sum += num;
-
-																		}
-
-										}
-
-
-
-					printf("%d\n", sum);
-
-
-
-						return (0);
-
+	return (0);
 }
