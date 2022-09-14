28ae3b9d524960556f820dff2c3fe1b458af09b2
motayomi <adesiyanolutayo@gmail.com>
2022-09-13 17:01:12 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ *  * print_alphabet - print alphabet in lower case
+ *
+ *   *Return: void
+ *
+ *    */
+
+
+
+void print_alphabet(void)
+
+{
+
+		char lowercase;
+
+
+
+			for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+
+						_putchar (lowercase);
+
+
+
+				_putchar('\n');
+
+}
