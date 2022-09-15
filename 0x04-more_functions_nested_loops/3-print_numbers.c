79e3166d0abbe833f4acac1e410f0041e6590e28
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-16 00:03:56 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,31 +1,16 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_numbers - prints number
- *
- *   */
-
-
+ * print_numbers - prints number
+ */
 
 void print_numbers(void)
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 48; i < 58; i++)
-
-					{
-
-								_putchar(i);
-
-									}
-
-				_putchar('\n');
-
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
