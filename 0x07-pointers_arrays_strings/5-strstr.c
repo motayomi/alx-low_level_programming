8c0d3ca983a8ad02ca71c4ea89ff13f5c0eb00bc
ADESIYAN OLUTAYO MOBOLAJI <106827609+motayomi@users.noreply.github.com>
2022-09-30 10:19:37 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,55 +1,28 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _strstr - Entry point
- *
- *   * @haystack: input
- *
- *    * @needle: input
- *
- *     * Return: Always 0 (Success)
- *
- *      */
-
-
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
 
 char *_strstr(char *haystack, char *needle)
-
 {
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
 
-		for (; *haystack != '\0'; haystack++)
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
 
-				{
+		if (*p == '\0')
+			return (haystack);
+	}
 
-							char *l = haystack;
-
-									char *p = needle;
-
-
-
-											while (*l == *p && *p != '\0')
-
-														{
-
-																		l++;
-
-																					p++;
-
-																							}
-
-
-
-													if (*p == '\0')
-
-																	return (haystack);
-
-														}
-
-
-
-			return (0);
-
+	return (0);
 }
